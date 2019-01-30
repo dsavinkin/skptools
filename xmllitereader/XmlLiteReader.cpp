743553d5814410863ed3b7707703a8b1a2fcc50b
Dmitry Savinkin <dsavinkin@gmail.com>
2019-01-30 16:29:55 +0200

    Added copy of .dll files.

--- xmllitereader/XmlLiteReader.cpp
+++ xmllitereader/XmlLiteReader.cpp
@@ -19,7 +19,19 @@
 #include <shlwapi.h>
 
 #include <SketchUpAPI/common.h>
+#include <SketchUpAPI/geometry.h>
+#include <SketchUpAPI/initialize.h>
+#include <SketchUpAPI/unicodestring.h>
+#include <SketchUpAPI/model/model.h>
+#include <SketchUpAPI/model/entities.h>
+#include <SketchUpAPI/model/face.h>
+#include <SketchUpAPI/model/edge.h>
+#include <SketchUpAPI/model/vertex.h>
 
+#include <SketchUpAPI/model/component_instance.h>
+#include <SketchUpAPI/model/component_definition.h>
+#include <SketchUpAPI/model/group.h>
+#include <vector>
 
 #pragma warning(disable : 4127)  // conditional expression is constant
 #define CHKHR(stmt)             do { hr = (stmt); if (FAILED(hr)) goto CleanUp; } while(0)
@@ -74,6 +86,44 @@ HRESULT WriteAttributes(IXmlReader* pReader)
         }
     }
     return hr;
+}
+
+int write_new_model()
+{
+    // Always initialize the API before using it
+    SUInitialize();
+    // Create an empty model
+    SUModelRef model = SU_INVALID;
+    SUResult res = SUModelCreate(&model);
+    // It's best to always check the return code from each SU function call.
+    // Only showing this check once to keep this example short.
+    if (res != SU_ERROR_NONE)
+        return 1;
+    // Get the entity container of the model
+    SUEntitiesRef entities = SU_INVALID;
+    SUModelGetEntities(model, &entities);
+    // Create a loop input describing the vertex ordering for a face's outer loop
+    SULoopInputRef outer_loop = SU_INVALID;
+    SULoopInputCreate(&outer_loop);
+    for (size_t i = 0; i < 4; ++i) {
+        SULoopInputAddVertexIndex(outer_loop, i);
+    }
+    // Create the face
+    SUFaceRef face = SU_INVALID;
+    SUPoint3D vertices[4] = { { 0,   0,   0 },
+        { 100, 100, 0 },
+        { 100, 100, 100 },
+        { 0,   0,   100 } };
+    SUFaceCreate(&face, vertices, &outer_loop);
+    // Add the face to the entities
+    SUEntitiesAddFaces(entities, 1, &face);
+    // Save the in-memory model to a file
+    SUModelSaveToFile(model, "new_model.skp");
+    // Must release the model or there will be memory leaks
+    SUModelRelease(&model);
+    // Always terminate the API when done using it
+    SUTerminate();
+    return 0;
 }
 
 int __cdecl wmain(int argc, _In_reads_(argc) WCHAR* argv[])
